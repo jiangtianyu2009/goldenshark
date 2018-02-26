7f31c3d189658b5732c7201b925e79e3b71b640b
jiangtianyu <jiangtianyu2009@hotmail.com>
2018-02-26 11:23:03 +0800

    Update Webhook Url Handler
    Update webhook url handler.

--- jav.py
+++ jav.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import request
 from flask import jsonify
 from scrapinghub import ScrapinghubClient
+import subprocess
 
 app = Flask(__name__)
 
@@ -14,6 +15,14 @@ def hello():
 @app.route('/thzdetails', methods=['GET', 'POST'])
 def getdetail():
     if request.method == 'GET':
-        return 'Ha Ha Ha, Get Details.'
+        return 'Update. Ha Ha Ha, Get Details.'
     if request.method == 'POST':
         return 'POSTPOSTPOST...'
+
+
+@app.route('/updatecode', methods=['POST'])
+def updatecode():
+    if request.method == 'POST':
+        output = subprocess.check_output(
+            ['git', '-C', r'/home/GoldenShark', 'pull'])
+        return output
---
+++ test.py
@@ -0,0 +1,4 @@
+import subprocess
+
+res = subprocess.check_output(['git', '-C', r'C:\jty\WorkSpace\GoldenShark', 'pull'])
+print(res)
