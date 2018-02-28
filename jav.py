35542aa0ab46b04bc07af9296139ad000c61bb82
jiangtianyu <jiangtianyu2009@hotmail.com>
2018-02-28 11:14:41 +0800

    Add Fetch Code List Function
    Add fetchcodelist function.

--- jav.py
+++ jav.py
@@ -26,3 +26,23 @@ def updatecode():
         output = subprocess.check_output(
             ['git', '-C', r'/home/GoldenShark', 'pull'])
         return output
+
+
+@app.route('/codelist', methods=['GET'])
+def fetchcodelist():
+    if request.method == 'GET':
+        codelist = []
+        apikey = '11befd9da9304fecb83dfa114d1926e9'
+        client = ScrapinghubClient(apikey)
+        project = client.get_project(252342)
+
+        for job in list(project.jobs.iter_last(spider='myspider', state='finished')):
+            codejob = job
+
+        print(codejob['key'])
+        lastcodejob = project.jobs.get(codejob['key'])
+
+        for item in lastcodejob.items.iter():
+            codelist.append(item)
+        print(codelist)
+        return jsonify(codelist)
--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import subprocess
 
-res = subprocess.check_output(['git', '-C', r'C:\jty\WorkSpace\GoldenShark', 'pull'])
+res = subprocess.check_output(
+    ['git', '-C', r'C:\jty\WorkSpace\GoldenShark', 'pull'])
 print(res)
