d0afe1a78028f6aa737a5ed885c76412f92e7bfd
jiangtianyu <jiangtianyu2009@hotmail.com>
2018-04-11 03:25:20 +0800

    Change URL In Json File
    Change urls in json files.

--- codelist.py
+++ codelist.py
@@ -18,29 +18,33 @@ lastcodejob = project.jobs.get(codejob['key'])
 codecounter = 0
 pagesection = 20
 pagecounter = 101
+imgbaseurl = 'https://www.goldenshark.xyz/images/'
 
 for item in lastcodejob.items.iter():
-    print()
-    print('Code: ' + item['code'])
-    print('Imgf: ' + item['imgf'])
-    if not os.path.exists(r'/home/GoldenShark/static/images/' + item['code'] + ".jpg"):
-        print('Downloading ' + item['code'] + ".jpg from " + item['imgf'])
-        urllib.request.urlretrieve(
-            item['imgf'], r'/home/GoldenShark/static/images/' + item['code'] + ".jpg")
-    else:
-        print(item['code'] + ".jpg exist.")
-    if codecounter < pagesection:
-        codelist.append(item)
-        codecounter = codecounter + 1
-    else:
-        codelistobj = json.dumps(codelist)
-        codelistfile = open(r'/home/GoldenShark/codelist/' +
-                            str(pagecounter) + r'.json', 'w')
-        codelistfile.write(codelistobj)
-        codelistfile.close()
-        codelist.clear()
-        codecounter = 0
-        pagecounter = pagecounter + 1
+
+    if item['imgf'] is not None and 'http' in item['imgf']:
+        print()
+        print('Code: ' + item['code'])
+        print('Imgf: ' + item['imgf'])
+        if not os.path.exists(r'/home/GoldenShark/static/images/' + item['code'] + ".jpg"):
+            print('Downloading ' + item['code'] + ".jpg from " + item['imgf'])
+            urllib.request.urlretrieve(
+                item['imgf'], r'/home/GoldenShark/static/images/' + item['code'] + ".jpg")
+        else:
+            print(item['code'] + ".jpg exist.")
+        item['imgf'] = imgbaseurl + item['code'] + ".jpg"
+        if codecounter < pagesection:
+            codelist.append(item)
+            codecounter = codecounter + 1
+        else:
+            codelistobj = json.dumps(codelist)
+            codelistfile = open(r'/home/GoldenShark/codelist/' +
+                                str(pagecounter) + r'.json', 'w')
+            codelistfile.write(codelistobj)
+            codelistfile.close()
+            codelist.clear()
+            codecounter = 0
+            pagecounter = pagecounter + 1
 
 codelistobj = json.dumps(codelist)
 codelistfile = open(r'/home/GoldenShark/codelist/' +
