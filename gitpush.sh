8ae952ec8ff3c5d4a36bcc09a21e02f7818ae755
jiangtianyu <jiangtianyu2009@hotmail.com>
2018-04-19 09:24:39 +0800

    Update Gitpush.sh
    Update gitpush.sh

--- gitpush.sh
+++ gitpush.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+git -C /home/bsonnier.github.io/ pull
+
 git -C /home/bsonnier.github.io/ add -A
 
-git -C /home/bsonnier.github.io/ commit -m "$(date)" -m "$(git status)"
+git -C /home/bsonnier.github.io/ commit -m "$(date)" -m "$(git -C /home/bsonnier.github.io/ status)"
 
 git -C /home/bsonnier.github.io/ push
