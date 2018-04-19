19544af4ca1d1498476ef3bce32cb9175b54ad55
jiangtianyu <jiangtianyu2009@hotmail.com>
2018-04-19 15:45:25 +0800

    Add Ranking
    Add ranking.

--- test.py
+++ test.py
@@ -6,5 +6,5 @@ client = algoliasearch.Client("P7KK27HK91", 'ea4c0f459be0c5aa47abf593071a119e')
 index = client.init_index("testind")
 batch = json.load(open(r'/home/GoldenShark/codelist/101.json'))
 index.add_objects(batch)
-
+index.set_settings({"customRanking": ["desc(href)"]})
 print(index.search("SSNI"))
