191ae12872e2681c791c36241c7bbdec1d5c61ad
jiangtianyu <jiangtianyu2009@hotmail.com>
2018-04-20 09:50:24 +0800

    Add New Index
    Add new.

--- codelist.py
+++ codelist.py
@@ -34,10 +34,10 @@ for item in lastcodejob.items.iter():
             urllib.request.urlretrieve(
                 item['imgf'], r'/home/GoldenShark/static/images/' + item['code'] + ".jpg")
             item['imgf'] = imgbaseurl + item['code'] + ".jpg"
+            asindex.add_object(item)
         else:
             print(item['code'] + ".jpg exist.")
             item['imgf'] = imgbaseurl + item['code'] + ".jpg"
-            asindex.add_object(item)
 
         codelistall.append(item)
         codelist.append(item)
