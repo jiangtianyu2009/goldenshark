87e54523af19db6e458cdad037ff8c117de32b3f
Tianyu Jiang <jiangtianyu2009@hotmail.com>
2020-11-26 14:04:36 +0800

    Update jav.py

--- jav.py
+++ jav.py
@@ -11,10 +11,13 @@ API_KEY = '11befd9da9304fecb83dfa114d1926e9'
 PROJECT_ID = '252342'
 
 
-@app.route("/")
-def index():
-    # return app.send_static_file('index.html')
-    return 'Hello, World!'
+def corsresponse(origres):
+    corsres = jsonify(origres)
+    corsres.headers['Access-Control-Allow-Origin'] = '*'
+    corsres.headers['Access-Control-Allow-Methods'] = 'GET, POST'
+    corsres.headers['Access-Control-Allow-Headers'] = 'x-requested-with,\
+        content-type'
+    return corsres
 
 
 @app.route('/favicon.ico')
@@ -24,29 +27,26 @@ def favicon():
                                mimetype='image/vnd.microsoft.icon')
 
 
-@app.route('/codelist', methods=['GET'])
+@app.route("/")
+def index():
+    # return app.send_static_file('index.html')
+    return 'Hello, World!'
+
+
+@app.route('/all_list', methods=['GET'])
 def fetchcodelist():
     if request.method == 'GET':
-        client = ScrapinghubClient(API_KEY)
+        client = ScrapinghubClient(API_KEY, use_msgpack=False)
         project = client.get_project(PROJECT_ID)
 
-        for jav_order_job in list(project.jobs.iter_last(
+        for job in list(project.jobs.iter_last(
                 spider='javorder', state='finished')):
-            javjob = jav_order_job
+            jav_order = job
 
-        print(javjob['key'])
-        jav_order_job = project.jobs.get(javjob['key'])
-
-        for item in jav_order_job.items.iter(count=1):
-            output = item
+        print(jav_order['key'])
+        jav_order_job = project.jobs.get(jav_order['key'])
+        output = []
+        for item in jav_order_job.items.iter():
+            output.append(item)
 
         return output
-
-
-def corsresponse(origres):
-    corsres = jsonify(origres)
-    corsres.headers['Access-Control-Allow-Origin'] = '*'
-    corsres.headers['Access-Control-Allow-Methods'] = 'GET, POST'
-    corsres.headers['Access-Control-Allow-Headers'] = 'x-requested-with,\
-        content-type'
-    return corsres
