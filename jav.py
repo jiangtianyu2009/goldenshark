05982bd092cf33387c7f768f39e39db5b125eff7
Tianyu Jiang <jiangtianyu2009@hotmail.com>
2020-11-27 12:30:57 +0800

    Update jav.py

--- jav.py
+++ jav.py
@@ -9,6 +9,8 @@ app = Flask(__name__, static_url_path='')
 
 API_KEY = '11befd9da9304fecb83dfa114d1926e9'
 PROJECT_ID = '252342'
+PAGINATION = 10
+BASE_PAGE = 1
 
 
 def corsresponse(origres):
@@ -33,20 +35,26 @@ def index():
     return 'Hello, World!'
 
 
-@app.route('/all_list', methods=['GET'])
-def fetchcodelist():
+@app.route('/list', methods=['GET'])
+def list_pagination():
     if request.method == 'GET':
         client = ScrapinghubClient(API_KEY, use_msgpack=False)
         project = client.get_project(PROJECT_ID)
+        para_page = BASE_PAGE
+        if request.args.get("page"):
+            para_page = int(request.args.get("page"))
 
         for job in list(project.jobs.iter_last(
                 spider='javorder', state='finished')):
             jav_order = job
-
-        print(jav_order['key'])
         jav_order_job = project.jobs.get(jav_order['key'])
         output = []
-        for item in jav_order_job.items.iter():
-            output.append(item)
+        for item in jav_order_job.items.list_iter(
+                start=(para_page - 1) * PAGINATION,
+                count=PAGINATION):
+            output = item
+        outdict = {}
+        for i, out in enumerate(output):
+            outdict[i] = out
 
-        return output
+        return outdict
