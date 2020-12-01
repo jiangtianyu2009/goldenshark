58308ef338f6dab50f80ae089347ff1cd768bea9
Tianyu Jiang <jiangtianyu2009@hotmail.com>
2020-12-01 12:37:37 +0800

    Update jav.py

--- jav.py
+++ jav.py
@@ -13,13 +13,13 @@ PAGINATION = 10
 BASE_PAGE = 1
 
 
-def corsresponse(origres):
-    corsres = jsonify(origres)
-    corsres.headers['Access-Control-Allow-Origin'] = '*'
-    corsres.headers['Access-Control-Allow-Methods'] = 'GET, POST'
-    corsres.headers['Access-Control-Allow-Headers'] = 'x-requested-with,\
+def cors_response(orig_res):
+    cors_res = jsonify(orig_res)
+    cors_res.headers['Access-Control-Allow-Origin'] = '*'
+    cors_res.headers['Access-Control-Allow-Methods'] = 'GET, POST'
+    cors_res.headers['Access-Control-Allow-Headers'] = 'x-requested-with,\
         content-type'
-    return corsres
+    return cors_res
 
 
 @app.route('/favicon.ico')
@@ -42,23 +42,54 @@ def goldenshark():
 @app.route('/list', methods=['GET'])
 def list_pagination():
     if request.method == 'GET':
+        output = []
+        outdict = {}
+        para_page = BASE_PAGE
         client = ScrapinghubClient(API_KEY, use_msgpack=False)
         project = client.get_project(PROJECT_ID)
-        para_page = BASE_PAGE
-        if request.args.get("page"):
-            para_page = int(request.args.get("page"))
-
         for job in list(project.jobs.iter_last(
                 spider='javorder', state='finished')):
             jav_order = job
         jav_order_job = project.jobs.get(jav_order['key'])
-        output = []
+
+        # Get para from URL like '/list?page=INT'
+        if request.args.get("page"):
+            para_page = int(request.args.get("page"))
         for item in jav_order_job.items.list_iter(
                 start=(para_page - 1) * PAGINATION,
                 count=PAGINATION):
             output = item
-        outdict = {}
         for i, out in enumerate(output):
             outdict[i] = out
 
-        return corsresponse(outdict)
+        # Return CORS response
+        return cors_response(outdict)
+
+
+@app.route('/search', methods=['GET'])
+def list_search():
+    if request.method == 'GET':
+        output = []
+        outdict = {}
+        client = ScrapinghubClient(API_KEY, use_msgpack=False)
+        project = client.get_project(PROJECT_ID)
+        for job in list(project.jobs.iter_last(
+                spider='javorder', state='finished')):
+            jav_order = job
+        jav_order_job = project.jobs.get(jav_order['key'])
+
+        # Get para from URL like '/search?name=XX'
+        # Get para from URL like '/search?makr=XX'
+        if request.args.get("name"):
+            search_name = request.args.get("name")
+            filters = [("name", "=", [search_name])]
+        if request.args.get("makr"):
+            search_makr = request.args.get("makr")
+            filters = [("makr", "=", [search_makr])]
+        for item in jav_order_job.items.iter(filter=filters):
+            output.append(item)
+        for i, out in enumerate(output):
+            outdict[i] = out
+
+        # Return CORS response
+        return cors_response(outdict)
